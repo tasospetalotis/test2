140f6cd2653c9e386ceac4c7fbf9a5ffee2e16b9
tasospetalotis <159777871+tasospetalotis@users.noreply.github.com>
2024-02-20 21:38:24 +0200

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -36,51 +36,28 @@ function addProduct() {
   }
 }
 
-function deleteCategoryOrProduct() {
-  var deleteSelector = document.getElementById('deleteSelector');
-  var selectedDeleteIndex = deleteSelector.selectedIndex;
+function deleteCategory() {
+  var categorySelector = document.getElementById('categorySelector');
+  var selectedCategoryIndex = categorySelector.selectedIndex;
 
-  if (selectedDeleteIndex !== -1) {
+  if (selectedCategoryIndex !== -1) {
     var categoriesAndProducts = JSON.parse(localStorage.getItem('categoriesAndProducts')) || [];
-    
-    if (deleteSelector.options[selectedDeleteIndex].classList.contains('category')) {
-      // Deleting a category
-      categoriesAndProducts.splice(selectedDeleteIndex, 1);
-    } else {
-      // Deleting a product
-      var selectedCategoryIndex = document.getElementById('categorySelector').selectedIndex;
-      categoriesAndProducts[selectedCategoryIndex].products.splice(selectedDeleteIndex, 1);
-    }
-
+    categoriesAndProducts.splice(selectedCategoryIndex, 1);
     localStorage.setItem('categoriesAndProducts', JSON.stringify(categoriesAndProducts));
     loadCategories();
   } else {
-    alert('Please select a category or product to delete.');
+    alert('Please select a category to delete.');
   }
 }
 
 function loadCategories() {
   var categorySelector = document.getElementById('categorySelector');
-  var deleteSelector = document.getElementById('deleteSelector');
   var categoriesAndProducts = JSON.parse(localStorage.getItem('categoriesAndProducts')) || [];
   categorySelector.innerHTML = '';
-  deleteSelector.innerHTML = '';
 
-  categoriesAndProducts.forEach(function (category, categoryIndex) {
+  categoriesAndProducts.forEach(function (category) {
     var option = document.createElement('option');
     option.text = category.name;
     categorySelector.add(option);
-
-    // Adding products to delete dropdown
-    var optgroup = document.createElement('optgroup');
-    optgroup.label = category.name;
-
-    category.products.forEach(function (product, productIndex) {
-      var productOption = document.createElement('option');
-      productOption.text = product.name;
-      optgroup.add(productOption);
-    });
-
-    deleteSelector.add(optgroup);
   });
 }
