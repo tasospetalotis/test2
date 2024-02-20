c80f20f9cb08abe8f961b577fe081eeef2cba806
tasospetalotis <159777871+tasospetalotis@users.noreply.github.com>
2024-02-20 18:40:50 +0200

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -13,37 +13,37 @@ function login() {
 
 function addCategory() {
   var categoryName = document.getElementById('categoryName').value;
-  var categorySelector = document.getElementById('categorySelector');
-  var option = document.createElement('option');
-  option.text = categoryName;
-  categorySelector.add(option);
+  var categoriesAndProducts = JSON.parse(localStorage.getItem('categoriesAndProducts')) || [];
+  var newCategory = { name: categoryName, products: [] };
+  categoriesAndProducts.push(newCategory);
+  localStorage.setItem('categoriesAndProducts', JSON.stringify(categoriesAndProducts));
+  loadCategories();
 }
 
 function addProduct() {
   var categorySelector = document.getElementById('categorySelector');
-  var selectedCategory = categorySelector.options[categorySelector.selectedIndex].text;
+  var selectedCategoryIndex = categorySelector.selectedIndex;
   var productName = document.getElementById('productName').value;
   var productPrice = document.getElementById('productPrice').value;
 
-  var categoriesAndProducts = JSON.parse(localStorage.getItem('categoriesAndProducts')) || {};
-  if (!categoriesAndProducts[selectedCategory]) {
-    categoriesAndProducts[selectedCategory] = [];
+  if (selectedCategoryIndex !== -1) {
+    var categoriesAndProducts = JSON.parse(localStorage.getItem('categoriesAndProducts')) || [];
+    categoriesAndProducts[selectedCategoryIndex].products.push({ name: productName, price: productPrice });
+    localStorage.setItem('categoriesAndProducts', JSON.stringify(categoriesAndProducts));
+    loadCategories();
+  } else {
+    alert('Please select a category.');
   }
-  categoriesAndProducts[selectedCategory].push({ name: productName, price: productPrice });
-
-  localStorage.setItem('categoriesAndProducts', JSON.stringify(categoriesAndProducts));
-
-  loadCategories();
 }
 
 function loadCategories() {
   var categorySelector = document.getElementById('categorySelector');
-  var categoriesAndProducts = JSON.parse(localStorage.getItem('categoriesAndProducts')) || {};
+  var categoriesAndProducts = JSON.parse(localStorage.getItem('categoriesAndProducts')) || [];
   categorySelector.innerHTML = '';
 
-  for (var category in categoriesAndProducts) {
+  categoriesAndProducts.forEach(function (category) {
     var option = document.createElement('option');
-    option.text = category;
+    option.text = category.name;
     categorySelector.add(option);
-  }
+  });
 }
