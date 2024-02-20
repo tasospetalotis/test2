064c1bf1354b20de5011b5648e24ddfce8e3734a
tasospetalotis <159777871+tasospetalotis@users.noreply.github.com>
2024-02-20 20:58:46 +0200

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -6,7 +6,6 @@ function login() {
     document.getElementById('loginForm').style.display = 'none';
     document.getElementById('dashboard').style.display = 'block';
     loadCategories();
-    loadCategoriesToDelete();
   } else {
     alert('Invalid username or password');
   }
@@ -19,7 +18,6 @@ function addCategory() {
   categoriesAndProducts.push(newCategory);
   localStorage.setItem('categoriesAndProducts', JSON.stringify(categoriesAndProducts));
   loadCategories();
-  loadCategoriesToDelete();
 }
 
 function addProduct() {
@@ -32,70 +30,20 @@ function addProduct() {
     var categoriesAndProducts = JSON.parse(localStorage.getItem('categoriesAndProducts')) || [];
     categoriesAndProducts[selectedCategoryIndex].products.push({ name: productName, price: productPrice });
     localStorage.setItem('categoriesAndProducts', JSON.stringify(categoriesAndProducts));
-    loadCategories();
+    loadCategories();  // Reload categories after adding the product
   } else {
     alert('Please select a category.');
   }
 }
 
-function deleteCategory() {
-  var categorySelectorToDelete = document.getElementById('categorySelectorToDelete');
-  var selectedCategoryIndex = categorySelectorToDelete.selectedIndex;
-
-  if (selectedCategoryIndex !== -1) {
-    var categoriesAndProducts = JSON.parse(localStorage.getItem('categoriesAndProducts')) || [];
-    categoriesAndProducts.splice(selectedCategoryIndex, 1);
-    localStorage.setItem('categoriesAndProducts', JSON.stringify(categoriesAndProducts));
-    loadCategories();
-    loadCategoriesToDelete();
-  } else {
-    alert('Please select a category to delete.');
-  }
-}
-
-function loadCategoriesToDelete() {
-  var categorySelectorToDelete = document.getElementById('categorySelectorToDelete');
+function loadCategories() {
+  var categorySelector = document.getElementById('categorySelector');
   var categoriesAndProducts = JSON.parse(localStorage.getItem('categoriesAndProducts')) || [];
-  categorySelectorToDelete.innerHTML = '';
+  categorySelector.innerHTML = '';
 
   categoriesAndProducts.forEach(function (category) {
     var option = document.createElement('option');
     option.text = category.name;
-    categorySelectorToDelete.add(option);
+    categorySelector.add(option);
   });
-}
-
-function loadProductsToDelete() {
-  var categorySelectorDeleteProduct = document.getElementById('categorySelectorDeleteProduct');
-  var selectedCategoryIndex = categorySelectorDeleteProduct.selectedIndex;
-  var productSelector = document.getElementById('productSelector');
-  productSelector.innerHTML = '';
-
-  if (selectedCategoryIndex !== -1) {
-    var categoriesAndProducts = JSON.parse(localStorage.getItem('categoriesAndProducts')) || [];
-    var selectedCategory = categoriesAndProducts[selectedCategoryIndex];
-
-    selectedCategory.products.forEach(function (product, index) {
-      var option = document.createElement('option');
-      option.text = product.name;
-      option.value = index;
-      productSelector.add(option);
-    });
-  }
-}
-
-function deleteProduct() {
-  var categorySelectorDeleteProduct = document.getElementById('categorySelectorDeleteProduct');
-  var selectedCategoryIndex = categorySelectorDeleteProduct.selectedIndex;
-  var productSelector = document.getElementById('productSelector');
-  var selectedProductIndex = productSelector.value;
-
-  if (selectedCategoryIndex !== -1 && selectedProductIndex !== '') {
-    var categoriesAndProducts = JSON.parse(localStorage.getItem('categoriesAndProducts')) || [];
-    categoriesAndProducts[selectedCategoryIndex].products.splice(selectedProductIndex, 1);
-    localStorage.setItem('categoriesAndProducts', JSON.stringify(categoriesAndProducts));
-    loadCategories();
-  } else {
-    alert('Please select a category and a product to delete.');
-  }
 }
